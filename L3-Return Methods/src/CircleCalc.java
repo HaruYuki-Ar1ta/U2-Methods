14ba0c54e14a7396ee0f302bfc86c45871729d0b
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-09-14 13:28:42 -0400

    L3-CircleCalc and ReturnMethod Complete

--- L3-Return Methods/src/CircleCalc.java
+++ L3-Return Methods/src/CircleCalc.java
@@ -1,15 +1,52 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class CircleCalc {
 
 
     public static void main(String[] args) {
 
         //create and run the program
+        //1.
+        double radius = gettingInput("What is the radius?");
+        //2.
+        double area = circArea(radius);
+        //3.
+        double circumference = circPerimeter(radius);
+        //4.
+        JOptionPane.showMessageDialog(null, displayResult(area, circumference));
 
     }
 
-    //re-write the method to calculate a circle's area using parameters and return statement
+    //Get inputs
+    public static double gettingInput(String prompt){
 
+        return Double.parseDouble(JOptionPane.showInputDialog(prompt));
+    }
+
+    //re-write the method to calculate a circle's area using parameters and return statement
+public static double circArea(double radius){
+
+        return Math.PI * Math.pow(radius, 2);
+}
 
     //re-write the method to calculate a circle's perimeter using parameters and return statement
+public static double circPerimeter(double radius) {
+
+
+        double circumference =  2 * Math.PI * radius;
+
+        return circumference;
+
+}
+    public static String displayResult(double area, double circumference) {
+
+        DecimalFormat round = new DecimalFormat("#.###");
+
+        String result = "The area is: " + round.format(area);
+        result += "\nThe circumference is: " + round.format(circumference);
+        return result;
+
+    }
 
 }
