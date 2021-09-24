bac020f47a12cca5d9b149fecbb10bdfcb75a71d
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-09-24 13:15:02 -0400

    L5 SimpleFrame Completed

--- L5-Swing Frames and Buttons/src/SimpleFrame.java
+++ L5-Swing Frames and Buttons/src/SimpleFrame.java
@@ -1,10 +1,29 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SimpleFrame
-{
+public class SimpleFrame {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
+        JFrame screen = new JFrame("This is a simple window");
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Click Me!!!");
+
+        screen.setSize(350,100);
+        screen.setLocationRelativeTo(null);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        screen.setVisible(true);
+
+        button.addActionListener(new ButtonListener());
+        panel.add(button);
+        screen.add(panel);
+
+    }
+
+    private static class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(null, "You got a dollar! ");
+        }
     }
 }
