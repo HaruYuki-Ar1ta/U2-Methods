598c6a073611e78f76aede11d3b5e39f4443286a
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-09-24 13:22:56 -0400

    L5 SimpleFrame Completed

--- L5-Swing Frames and Buttons/src/Quotes.java
+++ L5-Swing Frames and Buttons/src/Quotes.java
@@ -1,8 +1,32 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+
 public class Quotes {
 
     /*Create a GUI program with three buttons.
     Each button displays one of your favorite quotes.
     Hint: Use \n to make a new line.
      */
+    public static void main(String[] args) {
+        JFrame screen = new JFrame("Books?! Nope Quotes!!");
+        JPanel panel = new JPanel();
+
+        JButton button1 = new JButton("Click for option 1");
+        JButton button2 = new JButton("Click for option 2");
+        JButton button3 = new JButton("Click for option 3");
+
+        screen.setSize(300,300);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        screen.setVisible(true);
+
+        panel.add(butto1)
+        screen.add(panel);
+    }
+
+
+
+
+
 
 }
